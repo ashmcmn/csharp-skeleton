27fbd78a11f906d0b19d226ea3a522f83631dc44
Ash <amcmanamon1@sheffield.ac.uk>
2018-10-25 19:45:04 +0100

    Experimental Question 2 answer

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -1,11 +1,33 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System;
+using System.Linq;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
         public static int Answer(int[] cashflowIn, int[] cashflowOut)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            return (FindMin(cashflowIn.Concat(cashflowOut).ToArray())-1);
         }
+
+        static int FindMinRec(int[] arr, int i, int sumCalculated, int sumTotal) 
+        { 
+
+            if (i==0) 
+                return Math.Abs((sumTotal-sumCalculated) - sumCalculated); 
+  
+            return Math.Min(FindMinRec(arr, i-1, sumCalculated+arr[i-1], sumTotal), 
+                FindMinRec(arr, i-1, sumCalculated, sumTotal)); 
+        } 
+  
+        static int FindMin(int[] arr)
+        {
+            var n = arr.Length;
+            var sumTotal = 0; 
+            for (var i=0; i<n; i++) 
+                sumTotal += arr[i]; 
+  
+            return FindMinRec(arr, n, 0, sumTotal); 
+        } 
     }
 }
