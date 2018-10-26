4b4d45446ac17c226a508b803a9975dbd70d14e5
Ash <amcmanamon1@sheffield.ac.uk>
2018-10-26 01:09:06 +0100

    Made Question3 more efficient

--- Answers/Question3.cs
+++ Answers/Question3.cs
@@ -9,69 +9,69 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     public class Question3
     {
         public static int Answer(int numOfNodes, Edge[] edgeLists)
-        {
-            List<AdjacencyGraph<int, Edge<int>>> graphs = new List<AdjacencyGraph<int, Edge<int>>>();
-            List<List<int>> possibleSolutions = new List<List<int>>();
+                {
+                    List<AdjacencyGraph<int, Edge<int>>> graphs = new List<AdjacencyGraph<int, Edge<int>>>();
+                    List<List<int>> possibleSolutions = new List<List<int>>();
 
-            List<int> nodes = new List<int>();
-            for (int i = 1; i < numOfNodes + 1; i++)
-            {
-                nodes.Add(i);
-            }
-            
-            foreach (var combo in GetAllCombos(nodes))
-            {
-                AdjacencyGraph<int, Edge<int>> graph = new AdjacencyGraph<int, Edge<int>>();
-                foreach (var i in combo)
-                {
-                    graph.AddVertex(i);
-                }
-                foreach (var e in edgeLists)
-                {
-                    if (graph.ContainsVertex(e.EdgeA) && graph.ContainsVertex(e.EdgeB))
+                    List<int> nodes = Enumerable.Range(1, numOfNodes).ToList();
+                    
+                    int comboCount = (int) Math.Pow(2, nodes.Count) - 1;
+                    List<List<int>> combos = new List<List<int>>();
+                    for (int i = 1; i < comboCount + 1; i++)
                     {
-                        graph.AddEdge(new Edge<int>(e.EdgeA, e.EdgeB));
+                        combos.Add(new List<int>());
+                        for (int j = 0; j < nodes.Count; j++)
+                        {
+                            if ((i >> j) % 2 != 0)
+                                combos.Last().Add(nodes[j]);
+                        }
                     }
+                    
+                    
+                    for (var index = 0; index < combos.Count; index++)
+                    {
+                        var combo = combos[index];
+                        AdjacencyGraph<int, Edge<int>> graph = new AdjacencyGraph<int, Edge<int>>();
+                        for (var index1 = 0; index1 < combo.Count; index1++)
+                        {
+                            var i = combo[index1];
+                            graph.AddVertex(i);
+                        }
+
+                        bool dontAdd = false;
+                        for (var i = 0; i < edgeLists.Length; i++)
+                        {
+                            var e = edgeLists[i];
+                            if (graph.ContainsVertex(e.EdgeA) && graph.ContainsVertex(e.EdgeB))
+                            {
+                                dontAdd = true;
+                            }
+                        }
+
+                        if (!dontAdd)
+                        {
+                            graphs.Add(graph);
+                        }
+                        
+                    }
+
+                    AdjacencyGraph<int, Edge<int>> selectedGraph = new AdjacencyGraph<int, Edge<int>>();
+                    foreach (var graph in graphs.ToList())
+                    {
+                        if (graph.Edges.Any() || graph.VertexCount == 1)
+                        {
+                            graphs.Remove(graph);
+                        }
+                        else
+                        {
+                            if (graph.VertexCount > selectedGraph.VertexCount) selectedGraph = graph;
+                        }
+                    }
+
+                    int X = selectedGraph.VertexCount;
+                    int Y = numOfNodes - X;
+                    
+                    return X - Y;
                 }
-                graphs.Add(graph);
-            }
-            
-            AdjacencyGraph<int, Edge<int>> selectedGraph = new AdjacencyGraph<int, Edge<int>>();
-            foreach (var graph in graphs.ToList())
-            {
-                if (graph.Edges.Any() || graph.VertexCount == 1)
-                {
-                    graphs.Remove(graph);
-                }
-                else
-                {
-                    Console.WriteLine(string.Join(", ", graph.Vertices));
-                    if (graph.VertexCount > selectedGraph.VertexCount) selectedGraph = graph;
-                }
-            }
-            
-            int X = selectedGraph.VertexCount;
-            int Y = numOfNodes - X;
-            
-            Console.WriteLine(X+ ", " + Y);
-            return X - Y;
-        }
-        
-        public static List<List<T>> GetAllCombos<T>(List<T> list)
-        {
-            List<List<T>> result = new List<List<T>>();
-            result.Add(new List<T>());
-            result.Last().Add(list[0]);
-            if (list.Count == 1)
-                return result;
-            List<List<T>> tailCombos = GetAllCombos(list.Skip(1).ToList());
-            tailCombos.ForEach(combo =>
-            {
-                result.Add(new List<T>(combo));
-                combo.Add(list[0]);
-                result.Add(new List<T>(combo));
-            });
-            return result;
-        }
     }
 }
