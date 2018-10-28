1ce00d3abf384a4d0f9a644e03576b3e757291d5
Ashley McManamon <amcmanamon1@sheffield.ac.uk>
2018-10-28 20:52:36 +0000

    Update Question6.cs

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -1,11 +1,41 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuickGraph;
+using QuickGraph.Algorithms;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question6
     {
+        static AdjacencyGraph<int, Edge<int>> graph = new AdjacencyGraph<int, Edge<int>>();
+        static Dictionary<Edge<int>, double> costs = new Dictionary<Edge<int>, double>();
+        
         public static int Answer(int numOfServers, int targetServer, int[,] connectionTimeMatrix)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            for (int i = 0; i < connectionTimeMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < numOfServers; j++)
+                {
+                    var edge = new Edge<int>(i, j);
+                    graph.AddVerticesAndEdge(edge);
+                    costs.Add(edge, connectionTimeMatrix[i,j]);
+                }
+            }
+            
+            var edgeCost = AlgorithmExtensions.GetIndexer(costs);
+            var tryGetPath = graph.ShortestPathsDijkstra(edgeCost, 0);
+
+            IEnumerable<Edge<int>> path;
+            tryGetPath(targetServer, out path);
+
+            double cost = 0;
+            foreach (var edge in path)
+            {
+                cost += costs[edge];
+            }
+
+            return Convert.ToInt32(cost);
         }
     }
 }
